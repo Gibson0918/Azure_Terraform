1b255909172c4616bcaf19737c6adb685f9b861a
Gibson Yip (NCS) <gibson.yip@ncs.com.sg>
2025-01-14 11:10:56 +0800

    shift providers logic back to main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,22 @@ terraform {
       version = "~>3.0"
     }
   }
+
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "Gibson_Azure_Learning"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "Gibson_Azure_Learning-workspace"
+    }
+  }
 }
 
 provider "azurerm" {
   features {}
 }
+
 
 resource "azurerm_resource_group" "mtc-rg" {
   name     = "mtc-resource"
